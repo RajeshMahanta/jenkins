04f44e91b2ca5f47c07a44a6889036e861daf1e2
Rajesh Mahanta <rajlens@gmail.com>
2019-06-27 09:54:03 +0000

    modified files

--- vars.tf
+++ vars.tf
@@ -2,10 +2,10 @@
 //you can either enter your access/secrect keys in here, or otherwise you can create a file with name terraform.tfvars with these values.
 
 
-#variable "aws_access_key" {
-#    description = "Your AWS Access Key"
-#}
+variable "aws_access_key" {
+    description = "Your AWS Access Key"
+}
 
-#variable "aws_secret_key" {
-#    description = "Your AWS Secret Key"
-#}
+variable "aws_secret_key" {
+    description = "Your AWS Secret Key"
+}
