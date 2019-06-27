8055be93929e4bf8e0c5c8e35df7a723472edcc2
Rajesh Mahanta <rajlens@gmail.com>
2019-06-27 10:02:09 +0000

    modified files

---
+++ variables.secret.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+ description = "The AWS access key."
+ default     = "AKIAQREMEZ6EKXOFJ2FU"
+}
+variable "aws_secret_key" {
+ description = "The AWS secret key."
+ default     = "Quu+ngCt2NAktSKqMpwcmPnH5Ivxek/GJ7ehOtkN"
+}
+variable "region" {
+ default = "eu-west-2"
+}
